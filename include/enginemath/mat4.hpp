1047b32a910b77e19fd1e86fb378a59c6a23077d
AdrianYip1 <yipadrian1119@gmail.com>
2026-04-16 20:52:07 -0400

    feat: implement Mat4 x Vec4 multiplcation operator

--- include/enginemath/mat4.hpp
+++ include/enginemath/mat4.hpp
@@ -25,13 +25,23 @@ namespace enginemath {
 
 
         // important matrices
-        static constexpr Mat4 zeroMatrix() noexcept { return Mat4(Vec4(0.0f), Vec4(0.0f), Vec4(0.0f), Vec4(0.0f) ); }
-        static constexpr Mat4 identityMatrix() noexcept { 
+        static constexpr Mat4 zero() noexcept { return Mat4(Vec4(0.0f), Vec4(0.0f), Vec4(0.0f), Vec4(0.0f) ); }
+        static constexpr Mat4 identity() noexcept { 
             return Mat4(
-            Vec4(1.0f, 0.0f, 0.0f, 0.0f), 
-            Vec4(0.0f, 1.0f, 0.0f, 0.0f), 
-            Vec4(0.0f, 0.0f, 1.0f, 0.0f), 
-            Vec4(0.0f, 0.0f, 0.0f, 1.0f) ); }
+                Vec4(1.0f, 0.0f, 0.0f, 0.0f), 
+                Vec4(0.0f, 1.0f, 0.0f, 0.0f), 
+                Vec4(0.0f, 0.0f, 1.0f, 0.0f), 
+                Vec4(0.0f, 0.0f, 0.0f, 1.0f) ); }
+
+        //math
+
+        Vec4 operator*(const Vec4& right) const {
+            return Vec4(
+                (*this).c0.x * right.x + (*this).c1.x * right.y + (*this).c2.x * right.z + (*this).c3.x * right.w,
+                (*this).c0.y * right.x + (*this).c1.y * right.y + (*this).c2.y * right.z + (*this).c3.y * right.w,
+                (*this).c0.z * right.x + (*this).c1.z * right.y + (*this).c2.z * right.z + (*this).c3.z * right.w,
+                (*this).c0.w * right.x + (*this).c1.w * right.y + (*this).c2.w * right.z + (*this).c3.w * right.w );
+        }
         
     }
 
