bb68b73d0ee74bcb7b668ea5a9ae59221110258b
Adrian Yip <yipadrian1119@gmail.com>
2026-04-26 22:39:22 -0400

    feat: cleaned up code with std::, constexpr, and noexcepts for the scale and rotate functions.

--- include/enginemath/mat4.hpp
+++ include/enginemath/mat4.hpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include "vec4.hpp"
 
+
 namespace enginemath {
 
     struct Mat4 {
@@ -35,7 +36,7 @@ namespace enginemath {
             
         // matrix transforms 
         //translation
-        constexpr Mat4 translationM(const float x, const float y, const float z) {
+        static constexpr Mat4 translationM(const float x, const float y, const float z) {
             return Mat4(
                 Vec4(1.0f, 0.0f, 0.0f, 0.0f),
                 Vec4(0.0f, 1.0f, 0.0f, 0.0f),
@@ -43,7 +44,7 @@ namespace enginemath {
                 Vec4(x, y, z, 1.0f) ); }
 
         //scale
-        Mat4 scaleM(const float x, const float y, const float z) {
+        static constexpr Mat4 scaleM(const float x, const float y, const float z) noexcept {
             return Mat4(
                 Vec4(x, 0.0f, 0.0f, 0.0f),
                 Vec4(0.0f, y, 0.0f, 0.0f),
@@ -51,24 +52,24 @@ namespace enginemath {
                 Vec4(0.0f, 0.0f, 0.0f, 1.0f) ); }
 
         //rotation (theta is in radians)
-        Mat4 rotateX(const float theta) {
+        static Mat4 rotateX(const float theta) noexcept {
             return Mat4(
                 Vec4(1.0f, 0.0f, 0.0f, 0.0f),
-                Vec4(0.0f, cos(theta), sin(theta), 0.0f),
-                Vec4(0.0f, -sin(theta), cos(theta), 0.0f),
+                Vec4(0.0f, std::cos(theta), std::sin(theta), 0.0f),
+                Vec4(0.0f, -std::sin(theta), std::cos(theta), 0.0f),
                 Vec4(0.0f, 0.0f, 0.0f, 1.0f) ); }
 
-        Mat4 rotateY(const float theta) {
+        static Mat4 rotateY(const float theta) noexcept {
             return Mat4(
-                Vec4(cos(theta), 0.0f, -sin(theta), 0.0f),
+                Vec4(std::cos(theta), 0.0f, -std::sin(theta), 0.0f),
                 Vec4(0.0f, 1.0f, 0.0f, 0.0f),
-                Vec4(sin(theta), 0.0f, cos(theta), 0.0f),
+                Vec4(std::sin(theta), 0.0f, std::cos(theta), 0.0f),
                 Vec4(0.0f, 0.0f, 0.0f, 1.0f) ); }
 
-        Mat4 rotateZ(const float theta) {
+        static Mat4 rotateZ(const float theta) noexcept {
             return Mat4(
-                Vec4(cos(theta), sin(theta), 0.0f, 0.0f),
-                Vec4(-sin(theta), cos(theta), 0.0f, 0.0f),
+                Vec4(std::cos(theta), std::sin(theta), 0.0f, 0.0f),
+                Vec4(-std::sin(theta), std::cos(theta), 0.0f, 0.0f),
                 Vec4(0.0f, 0.0f, 1.0f, 0.0f),
                 Vec4(0.0f, 0.0f, 0.0f, 1.0f) ); }
 
